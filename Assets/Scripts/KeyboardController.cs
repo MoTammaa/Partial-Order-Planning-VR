28153201c16fb1c8d083462aadd209d42a4e6425
Mohamed <mohamed.abotammaa@student.guc.edu.eg>
2024-04-29 00:41:20 +0300

    added Name, Keyboard & Controller), teleport areas, enviroment objs, New prefabs

---
+++ Assets/Scripts/KeyboardController.cs
@@ -0,0 +1,117 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+//
+// Purpose: Demonstrates the use of the controller hint system
+//
+//=============================================================================
+
+using UnityEngine;
+using System.Collections;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+
+//-------------------------------------------------------------------------
+public class KeyBoardController : MonoBehaviour
+{
+	private GameObject keyboardInputObject;
+
+	void Start()
+	{
+		keyboardInputObject = GameObject.Find("KeyboardInput");
+	}
+
+	public void WriteText(string text)
+	{
+		// get the text of the "Text" object that called this function through the onClick event
+
+
+		// get the object with name "NameCanvas" and the "Text" object that is a child of it 
+		GameObject nameText = GameObject.Find("NameCanvas").transform.Find("Text").gameObject;
+
+		// append the text to the "Text" object that is a child of the "NameCanvas" object
+		if (nameText != null)
+		{
+			string oldname = nameText.GetComponent<UnityEngine.UI.Text>().text;
+			if (oldname == "Your Name" || oldname == "Please Enter Your Name")
+			{
+				nameText.GetComponent<UnityEngine.UI.Text>().text = "";
+				oldname = "";
+			}
+			switch (text)
+			{
+				case "<<x":
+					if (!string.IsNullOrEmpty(oldname))
+						nameText.GetComponent<UnityEngine.UI.Text>().text = oldname[..^1];
+					break;
+				case "Edit":
+					// show the keyboard "KeyboardInput"
+					keyboardInputObject.SetActive(true);
+
+					// Remove the "Welcome" message
+					nameText.GetComponent<UnityEngine.UI.Text>().text = PlayerPrefs.GetString("PlayerName");
+
+					// change the text and listner of the Done_Edit button to "Done"
+					GameObject Edit = GameObject.Find("Done_Edit");
+					GameObject Edit_Text = Edit?.transform.Find("Text")?.gameObject;
+					if (Edit_Text.GetComponent<UnityEngine.UI.Text>() == null) print("Done_Edit button is null");
+					Edit_Text.GetComponent<UnityEngine.UI.Text>().text = "Done";
+					if (Edit.GetComponent<UnityEngine.UI.Button>() == null) print("Done_Edit button is null");
+					Edit.GetComponent<UnityEngine.UI.Button>().onClick.RemoveAllListeners();
+					Edit.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => WriteText("Done"));
+					break;
+				case "Done":
+					if (oldname == "")
+					{
+						nameText.GetComponent<UnityEngine.UI.Text>().text = "Please Enter Your Name";
+						break;
+					}
+					// save the name to the playerprefs
+					PlayerPrefs.SetString("PlayerName", oldname);
+					// hide the keyboard "KeyboardInput"
+					keyboardInputObject.SetActive(false);
+					// change the text and listner of the Done_Edit button to "Edit Name"
+					GameObject Done = GameObject.Find("Done_Edit");
+					GameObject Done_Text = Done?.transform.Find("Text")?.gameObject;
+					if (Done_Text.GetComponent<UnityEngine.UI.Text>() == null) print("Done_Edit button is null");
+					Done_Text.GetComponent<UnityEngine.UI.Text>().text = "Edit Name";
+					if (Done.GetComponent<UnityEngine.UI.Button>() == null) print("Done_Edit button is null");
+					Done.GetComponent<UnityEngine.UI.Button>().onClick.RemoveAllListeners();
+					Done.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => WriteText("Edit"));
+
+					// Welcome the player
+					nameText.GetComponent<UnityEngine.UI.Text>().text = "Welcome " + oldname + "!";
+
+					print("Player Name: " + PlayerPrefs.GetString("PlayerName"));
+					break;
+				case "Space":
+					text = " ";
+					if (oldname.Length < 14)
+						nameText.GetComponent<UnityEngine.UI.Text>().text += text;
+					break;
+				default:
+					if (oldname.Length < 14)
+						nameText.GetComponent<UnityEngine.UI.Text>().text += text;
+					break;
+			}
+		}
+		else
+		{
+			Debug.Log("NameText is null");
+		}
+
+
+	}
+
+	public void Hello()
+	{
+		Debug.Log("Hello");
+	}
+
+
+
+
+
+
+
+
+}
